fa0c0909beac0c5b1d4030790bc6b992db384c0f
learnordie <learnordie@yandex.com>
2018-08-13 16:51:03 +0000

    Simplify the code used for plotting various subplots in a single plot

--- pyplot.py
+++ pyplot.py
@@ -15,12 +15,13 @@ import matplotlib.pyplot as plt
 x = (1, 2, 3, 4)  # x values as a tuple
 y = [1, 4, 9, 16]  # y values as a list
 
+plt.figure()  # creates a new figure
 plt.plot(x, y)
 plt.xlabel("x-axis personalized label")
 plt.ylabel("y-axis personalized label")
 plt.show()
 
-
+plt.figure()  # creates a new figure
 plt.plot(x, y)  # by default, the third argument is 'b-' (blue line)
 plt.plot(x, y, 'ro')  # red dots
 plt.xlabel("x-axis personalized label")
@@ -29,6 +30,7 @@ plt.show()
 
 
 # We can be more explicit with the arguments:
+plt.figure()  # creates a new figure
 plt.plot(x, y, color='green', marker='o',
          linestyle='dashed', linewidth=2, markersize=12)
 plt.xlabel("x-axis personalized label")
@@ -42,6 +44,7 @@ plt.show()
 
 # Plotting a one-dimensional array:
 a1 = np.random.randint(0, 100, 10)
+plt.figure()  # creates a new figure
 plt.plot(a1)
 plt.xlabel("x-axis personalized label")
 plt.ylabel("y-axis personalized label")
@@ -49,6 +52,7 @@ plt.show()
 
 # Plotting a two-dimensional array:
 a2 = np.random.randint(0, 100, 10).reshape(2, 5)
+plt.figure()  # creates a new figure
 plt.plot(a2[0], a2[1])
 plt.xlabel("x-axis personalized label")
 plt.ylabel("y-axis personalized label")
@@ -56,9 +60,11 @@ plt.show()
 
 # Histogram of a one-dimensional numpy array:
 a3 = np.random.randint(0, 100, 50)
+plt.figure()  # creates a new figure
 plt.hist(a3)
 plt.show()
 
+plt.figure()  # creates a new figure
 plt.hist(a3, cumulative=True)
 plt.show()
 
@@ -67,23 +73,24 @@ plt.show()
 # Plotting various subplots in a single plot #
 # ========================================== #
 
-# Plotting various plots in the same figure:
 x = np.random.randint(0, 100, 10)
 
-fig, axes = plt.subplots(ncols=2, nrows=2)
-ax0, ax1, ax2, ax3 = axes.flatten()
+plt.figure()  # creates a new figure
 
-ax0.plot(x, x)
-ax0.set_title('Linear')
+plt.subplot(2, 2, 1)  # nrows, ncols, index
+plt.plot(x, x)
+plt.title("Linear")
 
-ax1.plot(x, x * x)
-ax1.set_title('Squared')
+plt.subplot(2, 2, 2)  # nrows, ncols, index
+plt.plot(x, x * x)
+plt.title("Squared")
 
-ax2.plot(x, x ** 3)
-ax2.set_title('Cubic')
+plt.subplot(2, 2, 3)  # nrows, ncols, index
+plt.plot(x, x ** 3)
+plt.title("Cubic")
 
-ax3.plot(x, x ** 4)
-ax3.set_title('Quartic')
+plt.subplot(2, 2, 4)  # nrows, ncols, index
+plt.plot(x, x ** 4)
+plt.title("Quartic")
 
-fig.tight_layout()
 plt.show()
